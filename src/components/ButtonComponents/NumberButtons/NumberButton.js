25996783840dab181469104aa308ccdbda6159ae
Nikema Prophet <nikema@gmail.com>
2019-07-16 15:52:41 -0700

    Number buttons displaying

--- src/components/ButtonComponents/NumberButtons/NumberButton.js
+++ src/components/ButtonComponents/NumberButtons/NumberButton.js
@@ -1,9 +1,13 @@
 import React from "react";
 
-const NumberButton = () => {
+const NumberButton = (props) => {
   return (
     <>
-      {/* Display a button element rendering the data being passed down from the parent container on props */}
+    {/* Display a button element rendering the data being passed down from the parent container on props */}
+    {console.log(props)}
+      <button>{props.number}</button>
     </>
   );
 };
+
+export default NumberButton
